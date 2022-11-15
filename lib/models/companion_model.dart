499039fcf08134548e0f5eb57d1c59c0448dc54e
samaa <samaasoltan6@gmail.com>
2022-11-15 15:26:08 +0200

    models

---
+++ lib/models/companion_model.dart
@@ -0,0 +1,11 @@
+class Companion {
+  String name;
+  String relationShip;
+  String img;
+
+  Companion({
+    required this.name,
+    required this.relationShip,
+    required this.img,
+  });
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class User {
+  String name;
+  String title;
+  String img;
+
+  User({
+    required this.name,
+    required this.title,
+    required this.img,
+  });
+}
