fec781d7387c88153b2702553e45f876ee6f7702
samaa <samaasoltan6@gmail.com>
2022-11-15 15:25:16 +0200

    companion list tile and user card design

---
+++ lib/widgets/companions_list_tile.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:task1/models/companion_model.dart';
+
+import '../constants.dart';
+
+class CompanionsListTile extends StatelessWidget {
+  Companion companion;
+  CompanionsListTile({
+    required this.companion,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 47,
+          height: 47,
+          clipBehavior: Clip.antiAlias,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+          ),
+          child: Image.asset(
+            'assets/images/blank-profile-picture.png',
+            height: 47,
+            width: 47,
+          ),
+        ),
+        const SizedBox(
+          width: 26,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              companion.name,
+              style: kTitleUserDetailsListTile,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: Text(
+                companion.relationShip,
+                style: kSubTitleUserDetailsListTile,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+// ListTile(
+// leading: Container(
+// width: 100,
+// height: 100,
+// clipBehavior: Clip.antiAlias,
+// decoration: const BoxDecoration(
+// shape: BoxShape.circle,
+// ),
+// child: Image.asset(
+// 'assets/images/blank-profile-picture.png',
+// height: 47,
+// width: 47,
+// ),
+// ),
+// title: const Text(
+// 'James Black',
+// style: kTitleUserDetailsListTile,
+// ),
+// subtitle: const Padding(
+// padding: EdgeInsets.only(top: 8.0),
+// child: Text(
+// 'Spouce',
+// style: kSubTitleUserDetailsListTile,
+// ),
+// ),
+// );
---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class UserCardListWidget extends StatelessWidget {
+  const UserCardListWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 5),
+      color: Colors.grey[900],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ListTile(
+          leading: Container(
+            width: 52,
+            height: 52,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Image.asset(
+              'assets/images/blank-profile-picture.png',
+              height: 100,
+              width: 100,
+            ),
+          ),
+          title: const Text(
+            'Brooklyn Merrylin Simmons Simmons',
+            maxLines: 1,
+            style: kTitleUserMainListTile,
+          ),
+          subtitle: const Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Text(
+              'US Senitor | USA - P',
+              style: kSubTitleUserMainListTile,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
